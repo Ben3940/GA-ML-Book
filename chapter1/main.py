9b55c75b4a80730245f5645562fedb67e843b13b
Ben3940 <bty2015@comcast.net>
2024-05-06 20:31:18 -0400

    Finished drawing paths that trace square approach to escape bag

--- chapter1/main.py
+++ chapter1/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pickle
 
 
 # Renders paper bag that turtle must escape
@@ -34,10 +35,50 @@ def draw_line():
         tur.forward(step)
 
 
+def draw_square(tur, size):
+    path = list()
+
+    for i in range(4):
+        # move turtle forward step size and then turn 90 degrees
+        tur.forward(size)
+        tur.left(90)
+        store_position_data(path, tur)
+    return path
+
+
+def draw_squares(number):
+    tur = turtle.Turtle()
+    path = list()
+
+    for i in range(1, number + 1):
+        # reset pen position to next square start point
+        tur.penup()
+        tur.goto(-i, -i)
+        tur.pendown()
+        # Draw square and add to complete path
+        path.extend(draw_square(tur, i * 2))
+
+    return path
+
+
+# Draw all squares and save to file
+def draw_squares_until_escaped(n):
+    tur = turtle.Turtle()
+    complete_path = draw_squares(n)
+    with open("data_square", "wb") as file:
+        pickle.dump(complete_path, file)
+
+
+def store_position_data(path, tur):
+    position = tur.position()
+    path.append([position[0], position[1], escaped(position)])
+
+
 if __name__ == "__main__":
     # Sets window size
     turtle.setworldcoordinates(-70.0, -70.0, 70.0, 70.0)
     draw_bag()
-    draw_line()
+    # draw_line()
+    draw_squares(10)
     # Prevents window from closing automatically
     turtle.mainloop()
